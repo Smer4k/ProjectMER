6f0122fb17476821b38f449df571d0885b93e08d
Smer4k <151877040+Smer4k@users.noreply.github.com>
2025-11-16 14:14:06 +0700

    Bump

--- Features/Objects/SchematicTeleportObject.cs
+++ Features/Objects/SchematicTeleportObject.cs
@@ -6,49 +6,46 @@ namespace ProjectMER.Features.Objects;
 public class SchematicTeleportObject : MonoBehaviour
 {
     private readonly Dictionary<Player, DateTime> _nextUsePerPlayer = new();
-	public string Id { get; set; }
-	public float Cooldown { get; set; } = 5f;
-	public List<string> Targets { get; set; } = [];
+    public string Id { get; set; }
+    public float Cooldown { get; set; } = 5f;
+    public List<string> Targets { get; set; } = [];
 
-	public SchematicTeleportObject? GetRandomTarget()
-	{
-		string targetId = Targets.RandomItem();
+    public SchematicTeleportObject? GetRandomTarget()
+    {
+        string targetId = Targets.RandomItem();
 
-		foreach (SchematicTeleportObject teleportObject in FindObjectsByType<SchematicTeleportObject>(FindObjectsInactive.Exclude, FindObjectsSortMode.None))
-		{
-			if (teleportObject.Id != targetId)
-			{
-				continue;
-			}
+        foreach (SchematicTeleportObject teleportObject in FindObjectsByType<SchematicTeleportObject>(
+                     FindObjectsInactive.Exclude, FindObjectsSortMode.None))
+        {
+            if (teleportObject.Id != targetId)
+            {
+                continue;
+            }
 
-			return teleportObject;
-		}
+            return teleportObject;
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public void OnTriggerEnter(Collider other)
-	{
-		Player? player = Player.Get(other.gameObject);
-		if (player is null)
-			return;
-		
-		if (_nextUsePerPlayer.TryGetValue(player, out DateTime nextUse) && nextUse > DateTime.Now)
-			return;
-		
-		SchematicTeleportObject? target = GetRandomTarget();
-		if (target == null)
-			return;
-		
+    public void OnTriggerEnter(Collider other)
+    {
+        Player? player = Player.Get(other.gameObject);
+        if (player is null)
+            return;
+
+        if (_nextUsePerPlayer.TryGetValue(player, out DateTime nextUse) && nextUse > DateTime.Now)
+            return;
+
+        SchematicTeleportObject? target = GetRandomTarget();
+        if (target == null)
+            return;
+
         DateTime newCooldown = DateTime.Now.AddSeconds(Cooldown);
         _nextUsePerPlayer[player] = newCooldown;
+        target._nextUsePerPlayer[player] = newCooldown;
 
-        if (!target._nextUsePerPlayer.ContainsKey(player))
-            target._nextUsePerPlayer[player] = newCooldown;
-        else
-            target._nextUsePerPlayer[player] = newCooldown;
-
-		player.Position = target.gameObject.transform.position;
-		player.LookRotation = target.gameObject.transform.eulerAngles;
-	}
+        player.Position = target.gameObject.transform.position;
+        player.LookRotation = target.gameObject.transform.eulerAngles;
+    }
 }
