2edefc29aa81466a3efc482d0acb8679b443ad54
Smer4k <151877040+Smer4k@users.noreply.github.com>
2026-06-07 19:39:42 +0700

    Bump

--- AudioProjectMER/AudioProjectMER.cs
+++ AudioProjectMER/AudioProjectMER.cs
@@ -15,7 +15,7 @@ public sealed class AudioProjectMER : Plugin<Config>
     public override string Name { get; } = "AudioProjectMER";
     public override string Description { get; } = "Audio module for ProjectMER";
     public override string Author { get; } = "Smer4k";
-    public override Version Version { get; } = new Version("1.0.0");
+    public override Version Version { get; } = new Version("1.0.1");
     public override Version RequiredApiVersion { get; } = LabApiProperties.CurrentVersion;
     
     public static AudioProjectMER Singleton { get; private set; }
